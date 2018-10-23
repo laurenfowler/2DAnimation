34b741115fde814e03d94d469d3eaf4d0a807aeb
Lauren Fowler <fowler_l@gsc_405_06.mercer.edu>
2018-10-23 19:12:45 -0400

    tree is drawn

---
+++ draw_tree.cpp
@@ -0,0 +1,34 @@
+#include "includes.h"
+#include "constants.h"
+#include "struct.h"
+#include "prototypes.h"
+
+using namespace std;
+void draw_tree(int &num_points, struct point * circ_points, struct point * trunk_points){
+
+	point pixel;
+	int cutoff;
+
+	pixel = *(trunk_points + 0);
+	cutoff = pixel.x;
+
+	glColor3f(0.0,0.0,0.0);
+    for(int i=0; i<num_points; i++){
+        pixel = *(circ_points + i);
+        glBegin(GL_POINTS);
+			if(pixel.x > cutoff){
+        		glVertex2i(pixel.x, pixel.y);
+			}
+        glEnd();
+        glFlush();
+    } 
+
+	glBegin(GL_LINE_STRIP);
+		for(int i=0; i<6; i++){
+			pixel = *(trunk_points + i);
+			glVertex2i(pixel.x, pixel.y);
+		}	
+	glEnd();
+	glFlush();
+
+}
