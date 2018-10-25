22266f9f3a8dd192f5b4c88086fcbf738a764ebe
Lauren Fowler <fowler_l@henry.mercer.edu>
2018-10-25 11:50:02 -0400

    added globals.h

---
+++ globals.h
@@ -0,0 +1,7 @@
+#include "constants.h"
+#include "struct.h"
+int points;
+struct point * circ = (struct point *) malloc(NUM_POINTS * sizeof(struct point));
+struct point * trunk = (struct point *) malloc(6 * sizeof(struct point));
+struct point * circ_points = (struct point *) malloc(NUM_POINTS * sizeof(struct point));
+struct point * trunk_points = (struct point *) malloc(6 * sizeof(struct point));
